21faa73e8f48946903a4744b393503163ac14bd2
BlBe <962069597@qq.com>
2016-07-03 16:07:06 +0800

    PDO学习步伐-160703
    注释了PDO的概念，安装，检测

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
-header("charset='utf-8'");
+header("charset='utf-8'");      //设置网页编码为UTF-8
 //phpinfo();
+/*
+ *  PDO是什么？
+ *  pdo是PHP Data Objects的简写意思为php数据处理对象，在php5.1已经发布php数据库连接层即pdo，
+ *  以后php6.0将默认开启pdo，pdo支持各种数据库，只要换个驱动即可。
+ *
+ *  如何安装PDO？
+ *  在PHP程序中的EXT文件夹中把需要的PDO_数据库类型.dll文件复制到PHP程序的根目录下（可能本来就
+ *  存在，也可能路径不同）,然后再PHP.INI文件中找到PDO_数据库类型.dll，将前面的冒号删除即可！
+ *
+ *  如何检查安装成功？
+ *  重启PHP服务后，使用phpinfo();函数来查看PDO这个选项，如果有你需要的数据库类型，则成功
+ */
 $pdo = new PDO("mysql:hosts=192.168.1.112;dbname=bean",'root','qwer12');    //数据库连接
 if(!$pdo){
     echo "数据库连接失败<br>";
