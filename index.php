964189edb33a960050dd570c10213fff401cd4fb
BlBe <962069597@qq.com>
2016-07-03 11:33:29 +0800

    PDO学习步伐-160703
    由于昨天的学习中测试出现错误，所以今天重新研究新的输出办法，而且测试成功，并记录下来

--- index.php
+++ index.php
@@ -1,28 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Bean
- * Date: 2016/7/2
- * Time: 23:43
- */
-$pdo = new PDO('mysql:hosts=127.0.0.1,dbname=bean','root','qwer12',array(PDO::ATTR_AUTOCOMMIT=>true));
-//创建持续性PDO数据库连接
-
-$sql = $pdo->query("select * from user");
-//执行查询语句，返回数组
-while($row = $sql->fetchAll()){
-    echo $row['user']."<br>";
-    echo $row['pass']."<hr>";
+header("charset='utf-8'");
+//phpinfo();
+$pdo = new PDO("mysql:hosts=192.168.1.112;dbname=bean",'root','qwer12');    //数据库连接
+if(!$pdo){
+    echo "数据库连接失败<br>";
 }
-//将SQL数组使用while循环输出
+$sql = $pdo->prepare('select * from user'); //使用prepare查询
+$sql->execute();                            //获取数值
+//$res = $sql->fetchAll();
+//print_r($res);
+echo "<p> <p> <p> <p> <p> <p> </p></p></p></p></p></p>";
+foreach($sql->fetchAll() as $res){      //fetchAll（）    输出
+    echo $res[1]."<br>".$res[2]."<hr>";
+}
+$pdo = null;    //数据库关闭
 
-$get = $pdo->exec("insert into user (user,pass) VALUES ('user','pass')");
-echo "加入$get条数据";
-//执行插入语句返回影响行数
 
-$del = $pdo->exec("delete from user where user='user'");
-echo "删除$del条数据";
-//执行删除语句返回影响条数
-
-$pdo = null ;
-//关闭数据库连接
